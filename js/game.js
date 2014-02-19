2752a1a539befd39ee24effcbc61c12b7116c858
Nicole Paz <nicole.j.paz@gmail.com>
2014-02-18 17:28:27 -0800

    Added function to place lines on the board. Temporary until the pieces are more distinct.

--- js/game.js
+++ js/game.js
@@ -4,10 +4,26 @@ $(document).ready(function() {
 
 function Main() {
   var gameCanvas = document.querySelector("#game-canvas") 
+  new Board(gameCanvas)
   new Piece(gameCanvas)
 }
 
+function Board(gameCanvas) {
+  var gameBoard = gameCanvas.getContext("2d")
+  makeBoardLines(gameBoard)
+}
 
+function makeBoardLines(gameBoard) {
+  for(var i = 0; i <= 500; i += 25) {
+    gameBoard.moveTo(i, 0)
+    gameBoard.lineTo(i, 500)
+    gameBoard.stroke()
+
+    gameBoard.moveTo(0, i)
+    gameBoard.lineTo(500, i)
+    gameBoard.stroke()
+  }
+}
 
 function Piece(gameCanvas) {
   var gamePiece = gameCanvas.getContext("2d")
