493d053cd834879a2a7dcbeca7c234b081ef4094
Nicole Paz <nicole.j.paz@gmail.com>
2014-02-18 18:17:40 -0800

    Moved color creation to another function.

--- js/game.js
+++ js/game.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
   GAME_PIECE_SIZE = 50
-  COLORS = ["#0c5da5", "#8106a9", "#e9fb00", "#ff9500"]
+  makeColorConstant()
+
   new Main()
 })
+
+function makeColorConstant() {
+  var blue = "#0c5da5"
+  var purple = "#8106a9"
+  var yellow = "#e9fb00"
+  var orange = "#ff9500"
+  COLORS = [blue, purple, yellow, orange]
+}
 
 function Main() {
   var gameCanvas = document.querySelector("#game-canvas") 
@@ -34,7 +43,8 @@ function Piece(gameCanvas) {
 
   for(var y = 0; y <= 500; y += GAME_PIECE_SIZE) {
     for(var x = 0; x <= 500; x += GAME_PIECE_SIZE) {
-      gamePiece.fillStyle = pickColor()
+      var color = pickColor()
+      gamePiece.fillStyle = color
       gamePiece.fillRect(x, y, GAME_PIECE_SIZE, GAME_PIECE_SIZE)
     }
   }
