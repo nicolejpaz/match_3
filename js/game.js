a2a40d490ddfe267b8ef4ac24c1e55fcd29df6c2
Nicole Paz <nicole.j.paz@gmail.com>
2014-02-18 19:46:35 -0800

    Moved piece logic to separate file.

--- js/game.js
+++ js/game.js
@@ -17,34 +17,3 @@ function Game() {
   var gameCanvas = document.querySelector("#game-canvas") 
   new Board(gameCanvas)
 }
-
-
-
-function buildPiece(piece, gameCanvas, x, y) {
-  piece.build(gameCanvas, x, y)
-  if (Piece.color() === COLORS[0]) {
-    piece.type = "blue"
-  } else if (Piece.color() === COLORS[1]) {
-    piece.type = "purple"
-  } else if (Piece.color() === COLORS[2]) {
-    piece.type = "yellow"
-  } else if (Piece.color() === COLORS[3]) {
-    piece.type = "orange"
-  }
-}
-
-var Piece = {
-  color: function() {
-    return pickColor()
-  },
-  build: function(gameCanvas, x, y) {
-    var gamePiece = gameCanvas.getContext("2d")
-
-    gamePiece.fillStyle = this.color()
-    gamePiece.fillRect(x, y, GAME_PIECE_SIZE, GAME_PIECE_SIZE)
-  }
-}
-
-function pickColor() {
-  return COLORS[Math.floor(Math.random() * 4)]
-}
