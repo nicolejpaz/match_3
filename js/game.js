19254bcd6df4771972a4c5d6519021f003527b73
Nicole Paz <nicole.j.paz@gmail.com>
2014-02-18 19:42:39 -0800

    Moved board logic to separate file.

--- js/game.js
+++ js/game.js
@@ -18,40 +18,7 @@ function Game() {
   new Board(gameCanvas)
 }
 
-// *** CREATE BOARD *** //
 
-function Board(gameCanvas) {
-  var boardPosition = createBoard(gameCanvas)
-
-  var gameBoard = gameCanvas.getContext("2d")
-  makeBoardLines(gameBoard)
-}
-
-function createBoard(gameCanvas) {
-  var boardPosition = []
-  
-  for(var pieceNumber = 0; pieceNumber <= 100; pieceNumber++) {
-    for(var y = 0; y <= 500; y += GAME_PIECE_SIZE) {
-      for(var x = 0; x <= 500; x += GAME_PIECE_SIZE) {
-        boardPosition[pieceNumber] = Piece
-        buildPiece(boardPosition[pieceNumber], gameCanvas, x, y)
-      }
-    }
-  }
-  return boardPosition
-}
-
-function makeBoardLines(gameBoard) {
-  for(var i = 0; i <= 500; i += GAME_PIECE_SIZE) {
-    gameBoard.moveTo(i, 0)
-    gameBoard.lineTo(i, 500)
-    gameBoard.stroke()
-    
-    gameBoard.moveTo(0, i)
-    gameBoard.lineTo(500, i)
-    gameBoard.stroke()
-  }
-}
 
 function buildPiece(piece, gameCanvas, x, y) {
   piece.build(gameCanvas, x, y)
