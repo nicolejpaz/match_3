d97e52cbefeb2d9694314449e86cf37af82b0c9e
Nicole Paz <nicole.j.paz@gmail.com>
2014-02-18 17:18:22 -0800

    Added gamePiece function.

--- js/game.js
+++ js/game.js
@@ -4,5 +4,13 @@ $(document).ready(function() {
 
 function Main() {
   var gameCanvas = document.querySelector("#game-canvas") 
-  
+  new Piece(gameCanvas)
+}
+
+
+
+function Piece(gameCanvas) {
+  var gamePiece = gameCanvas.getContext("2d")
+  gamePiece.fillStyle = "#ccc"
+  gamePiece.fillRect(0, 0, 25, 25)
 }
